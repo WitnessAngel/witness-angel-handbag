a7f02d50c81c80488dbde55101feb7b9dbca4de8
kryckely <ryck.k@hotmail.com>
2021-11-24 16:19:26 +0100

    Add i2sMicro support

--- main/appMain.c
+++ main/appMain.c
@@ -1,23 +1,28 @@
 #include <esp_err.h>
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #include "app.h"
-#include "camera.h"
-#include "esp_camera.h"
-#include "i2sMicro.h"
-#include "noVolatileStorage.h"
 #include "sdCard.h"
+#include "noVolatileStorage.h"
+#include "i2sMicro.h"
 
-static const char *TAG = "app_main";
+static const char *TAG = "appMain";
 
-static esp_err_t app_init(app_t *self)
-{
+
+static esp_err_t app_init(app_t *app) {
   esp_err_t err;
 
   if ((err = nvs_init())
-	  || (err = sdCard_init())
-	  || (err = i2sMicro_init(&self->i2sMicro))
-	  || (err = camera_init()))
+      /* || (err = appScreen_init(&app->screen)) */
+      /* || (err = esp_netif_init()) */
+      /* || (err = esp_event_loop_create_default())) */
+      /* || (err = multicastDNS_init()) */
+      /* || (err = appServer_init(app)) */
+      || (err = sdCard_init())
+	  || (err = i2sMicro_init(&app->i2sMicro)))
+  /* || (err = wifi_init(&app->wifi)) */
   {
     ESP_LOGE(TAG, "Error during app init : (%s) !\n", esp_err_to_name(err));
     return (err);
@@ -27,16 +32,11 @@ static esp_err_t app_init(app_t *self)
   return (ESP_OK);
 }
 
-void app_main()
+void app_main(void)
 {
 	static app_t app = {0};
-	/* esp_err_t err; */
 
 	ESP_ERROR_CHECK(app_init(&app));
-
-	ESP_LOGI(TAG, "App start !");
-
-	micro_record(&app, "/sdcard/test.wav");
-
-	ESP_LOGI(TAG, "App end !");
+	ESP_LOGI(TAG, "appMicro begin");
+	record(&app, "/sdcard/manu.wav");
 }
