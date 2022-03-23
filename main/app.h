6b98e656a97e13a6af102c2a422467fdadad6720
kryckely <ryck.k@hotmail.com>
2022-03-23 11:11:29 +0100

    FEAT: Add encrypted/decrypted function to first_init.c

--- main/app.h
+++ main/app.h
@@ -4,12 +4,28 @@
 
 #include "i2sMicro.h"
 #include "uart.h"
+#include "vector.h"
+
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/pk.h>
+#include <mbedtls/rsa.h>
 
 typedef struct app_s app_t;
 struct app_s
 {
     i2sMicro_t i2sMicro;
     uart_t uart;
+};
+
+typedef struct encrypted_uart_s encrypted_uart_t;
+struct encrypted_uart_s
+{
+    mbedtls_pk_context pk;
+    mbedtls_entropy_context entropy; /* entropy pool for seeding PRNG */
+    mbedtls_ctr_drbg_context drbg;   /* pseudo-random generator */
+    unsigned char buf_drbg[256];
+    /* t_vector* vec; */
 };
 
 void record(app_t* app, const char* fname);
--- main/first_init.c
+++ main/first_init.c
@@ -11,13 +11,89 @@
 #include <freertos/task.h>
 #include <stdlib.h>
 
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/pk.h>
+#include <mbedtls/rsa.h>
+
 #define TAG "first_init"
 
-/* CONFIG_ECHO_UART_PORT_NUM = 2
-   CONFIG_ECHO_UART_BAUD_RATE = 115200
-   CONFIG_ECHO_UART_RXD = 3
-   CONFIG_ECHO_UART_TXD = 1
-   CONFIG_ECHO_UART_RTS = 22 */
+/* CONFIG_UART_PORT_NUM = 2
+   CONFIG_UART_BAUD_RATE = 115200
+   CONFIG_UART_RXD = 3
+   CONFIG_UART_TXD = 1
+   CONFIG_UART_RTS = 22 */
+
+void encrypted_uart_init(encrypted_uart_t* encrypted_uart,
+                         const unsigned char* key, const size_t keylen)
+{
+    int ret = 0;
+    mbedtls_pk_init(&encrypted_uart->pk);
+
+    /*
+     * Read the RSA public key
+     */
+    if ((ret = mbedtls_pk_parse_public_key(&encrypted_uart->pk, key, keylen)) !=
+        0)
+    {
+        ESP_LOGE(
+            TAG,
+            " failed\n  ! mbedtls_pk_parse_public_keyfile returned -0x%04x\n",
+            -ret);
+    }
+
+    mbedtls_entropy_init(&encrypted_uart->entropy);
+    mbedtls_ctr_drbg_init(&encrypted_uart->drbg);
+
+    int pub_ret;
+    const char* pers = "encrypted_uart_communication";
+
+    /* Seed the PRNG using the entropy pool, and throw in our secret key as an
+     * additional source of randomness. */
+
+    pub_ret = mbedtls_ctr_drbg_seed(&encrypted_uart->drbg, mbedtls_entropy_func,
+                                    &encrypted_uart->entropy,
+                                    (const unsigned char*)pers, strlen(pers));
+
+    if (pub_ret != 0)
+    {
+        ESP_LOGE(TAG,
+                 "Mauvaise configuration de la fonction mbedtls_ctr_drbg_seed");
+    }
+
+    mbedtls_ctr_drbg_random(&encrypted_uart->drbg, encrypted_uart->buf_drbg,
+                            256);
+}
+
+void encrypt(encrypted_uart_t* encrypted_uart, const unsigned char* input,
+             unsigned char* output, size_t input_length, size_t output_length)
+{
+    int ret;
+
+    if ((ret = mbedtls_pk_encrypt(&encrypted_uart->pk, input, input_length,
+                                  output, &output_length, sizeof(output),
+                                  mbedtls_ctr_drbg_random,
+                                  &encrypted_uart->drbg)) != 0)
+    {
+        ESP_LOGE(TAG, " failed\n  ! mbedtls_pk_encrypt returned -0x%04x\n",
+                 -ret);
+    }
+}
+
+void decrypt(encrypted_uart_t* encrypted_uart, const unsigned char* input,
+             unsigned char* output, size_t input_length, size_t output_length)
+{
+    int ret;
+
+    if ((ret = mbedtls_pk_decrypt(&encrypted_uart->pk, input, input_length,
+                                  output, &output_length, sizeof(output),
+                                  mbedtls_ctr_drbg_random,
+                                  &encrypted_uart->drbg)) != 0)
+    {
+        ESP_LOGE(TAG, " failed\n  ! mbedtls_pk_decrypt returned -0x%04x\n",
+                 -ret);
+    }
+}
 
 static void rs_send(const int port, const char* str, size_t size)
 {
@@ -29,32 +105,7 @@ static void rs_send(const int port, const char* str, size_t size)
     }
 }
 
-/* static void rs_rpc_test(void) */
-/* { */
-
-/*     RPCJson_t request; */
-/*     RPCJson_t testing; */
-
-/*     rpc_json_create_request(&testing, "ping", 1); */
-/*     char* jsontesting_string = cJSON_Print(testing.rpc_json_obj); */
-/*     puts(jsontesting_string); */
-/*     char test[] = "{\"jsonrpc\": \"2.0\", \"method\": \"ping\", \"id\": 40}";
- */
-/*     puts(test); */
-/*     char test_2[] = "{\n\ */
-/* 	\"json_rpc\":	\"2.0\",\n\ */
-/* 	\"method\":	\"ping\",\n\ */
-/* 	\"id\":	1\n\ */
-/* }"; */
-/*     puts(test_2); */
-/*     if (!rpc_json_parse(&request, (char*)test)) */
-/*     { */
-/*         ESP_LOGE(TAG, "rpc json parse fail"); */
-/*         return; */
-/*     } */
-/* } */
-
-static void rs_rpc_response(t_vector* input)
+static void rs_rpc_response(uart_t* uart, t_vector* input)
 {
     RPCJson_t request;
     RPCJson_t response;
@@ -84,36 +135,36 @@ static void rs_rpc_response(t_vector* input)
     /*     rs_send(ECHO_UART_PORT, "\nError: Test\n", strlen("\nError:
      * Test\n")); */
 
-    /* if (!rpc_json_parse(&request, (char*)input->data)) */
-    /* { */
-    /*     rs_send(ECHO_UART_PORT, "\nError: 'rpc json parse fail'", */
-    /*             strlen("\nError: 'rpc json parse fail'")); */
-    /*     return; */
-    /* } */
-    /* if (!rpc_json_get_id(&request, &id)) */
-    /*     rs_send(ECHO_UART_PORT, "\nError:\nrpc json get fail", */
-    /*             strlen("\nError:\nrpc json get fail")); */
-    /* if (!rpc_json_create_response(&response, id, 42)) */
-    /* { */
-    /*     /\* rpc_json_create_error(&response, 0, 1, "rpc_json_parse error");
-     */
-    /*      *\/ */
-    /*     ESP_LOGE(TAG, "rpc json create response fail"); */
-    /*     rs_send(ECHO_UART_PORT, "\nError:\nrpc json create response fail", */
-    /*             strlen("\nError:\nrpc json create response fail")); */
-    /*     return; */
-    /* } */
-    /* char* json_string = cJSON_PrintUnformatted(response.rpc_json_obj); */
-    /* rs_send(ECHO_UART_PORT, json_string, strlen(json_string)); */
+    if (!rpc_json_parse(&request, (char*)input->data))
+    {
+        rs_send(uart->port_num, "\nError: 'rpc json parse fail'",
+                strlen("\nError: 'rpc json parse fail'"));
+        return;
+    }
+    if (!rpc_json_get_id(&request, &id))
+        rs_send(uart->port_num, "\nError:\nrpc json get fail",
+                strlen("\nError:\nrpc json get fail"));
+    if (!rpc_json_create_response(&response, id, 42))
+    {
+        /* rpc_json_create_error(&response, 0, 1, "rpc_json_parse error");
+         */
+        ESP_LOGE(TAG, "rpc json create response fail");
+        rs_send(uart->port_num, "\nError:\nrpc json create response fail",
+                strlen("\nError:\nrpc json create response fail"));
+        return;
+    }
+    char* json_string = cJSON_PrintUnformatted(response.rpc_json_obj);
+    rs_send(uart->port_num, json_string, strlen(json_string));
 }
 
 // uart_task with hardware flow control on UART
 static void uart_task(void* arg)
 {
     uart_t* uart = (uart_t*)arg;
+    encrypted_uart_t receive;
+    encrypted_uart_t send;
 
     ESP_LOGI(TAG, "UART start receive loop.\r\n");
-    /* rs_send(uart_num, "Start RS485 UART test.\r\n", 24); */
 
     while (1)
     {
@@ -125,40 +176,17 @@ static void uart_task(void* arg)
         // Write data back to UART
         if (len > 0)
         {
-            /* rs_send(uart->port_num, "\r\n", 2); */
-            /* char prefix[] = "RS485 Received: ["; */
-            /* rs_send(uart_num, prefix, (sizeof(prefix) - 1)); */
-            /* ESP_LOGI(TAG, "Received %u bytes:", len); */
-            /* printf("[ "); */
             t_vector* vec = vecnew(1, sizeof(uint8_t), 1);
-            /* char c[5] = {0}; */
             for (int i = 0; i < len; i++)
             {
-                /* sprintf(c, "0x%.2X", (uint8_t)data[i]); */
                 vecpush(vec, (const char*)&uart->data[i]);
-                /* rs_send(uart_num, (const char*)&data[i], 1); */
-                // Add a Newline character if you get a return charater from
-                // paste (Paste tests multibyte receipt/buffer)
-                /* if (data[i] == '\r') */
-                /* { */
-                /*     rs_send(uart_num, "\n", 1); */
-                /* } */
             }
-            /* rs_send(uart_num, (char*)data, len); */
-            /* ESP_LOGI(TAG, "%s", (char*)vec->data); */
-            /* char test[5] = {0}; */
-            /* sprintf(test, "%d", vec->length); */
-            /* rs_send(uart_num, test, strlen(test)); */
-            rs_send(uart->port_num, (char*)vec->data, vec->length);
-            rs_rpc_response(vec);
+            /* rs_send(uart->port_num, (char*)vec->data, vec->length); */
+            rs_rpc_response(uart, vec);
             vecfree(vec);
-            /* printf("] \n"); */
-            /* rs_send(uart_num, "]\r\n", 3); */
         }
         else
         {
-            /* Echo a "." to show we are alive while we wait for input */
-            /* rs_send(uart_num, ".", 1); */
             ESP_ERROR_CHECK(uart_wait_tx_done(uart->port_num, 2));
         }
     }
